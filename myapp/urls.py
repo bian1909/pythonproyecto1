9e84f2be53c2cad42a65c96c410244eefb0b15de
facundoseiber <davidsseiber@gmail.com>
2026-06-16 01:14:51 -0300

    Implementacion Completa

--- myapp/urls.py
+++ myapp/urls.py
@@ -8,6 +8,8 @@ urlpatterns = [
     path('estudiantes/', views.lista_estudiantes, name='estudiantes'),
     path('estudiantes/<int:pk>/', views.detalle_estudiante, name='detalle_estudiante'),
     path('profesores/', views.profesores, name='profesores'),
+    path('profesor/editar/<int:id>/', views.profesor_editar, name='profesorEditar'),       # NUEVA
+    path('profesor/eliminar/<int:id>/', views.profesor_eliminar, name='profesorEliminar'), # NUEVA
     path('cursos/', views.cursos, name='cursos'),
     path('entregables/', views.entregables, name='entregables'),
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+from django.db import models
 from .models import Estudiante, Profesor, Curso, Entregable
+from .forms import ProfesorForm
 
 # Vista para el index
 def index(request):
@@ -15,10 +17,38 @@ def detalle_estudiante(request, pk):
     estudiante = get_object_or_404(Estudiante, pk=pk)
     return render(request, 'myapp/estudiante_detail.html', {'estudiante': estudiante})
 
-# Vistas para Profesores
+# VISTAS PARA PROFESORES (CON EL CRUD NUEVO)
 def profesores(request):
-    profesores = Profesor.objects.all()
-    return render(request, 'myapp/profesores.html', {'profesores': profesores})
+    query = request.GET.get('q')
+    # Si el usuario busca algo en la barra, filtramos; si no, trae todos
+    if query:
+        profesores = Profesor.objects.filter(
+            models.Q(nombre__icontains=query) |
+            models.Q(apellido__icontains=query) |
+            models.Q(profesion__icontains=query)
+        )
+    else:
+        profesores = Profesor.objects.all()
+        
+    return render(request, 'myapp/profesores.html', {'profesores': profesores, 'query': query})
+
+def profesor_editar(request, id):
+    profesor = get_object_or_404(Profesor, id=id)
+    if request.method == 'POST':
+        form = ProfesorForm(request.POST, instance=profesor)
+        if form.is_valid():
+            form.save()
+            return redirect('myapp:profesores')
+    else:
+        form = ProfesorForm(instance=profesor)
+    return render(request, 'myapp/profesor_editar.html', {'form': form, 'profesor': profesor})
+
+def profesor_eliminar(request, id):
+    profesor = get_object_or_404(Profesor, id=id)
+    if request.method == 'POST':
+        profesor.delete()
+        return redirect('myapp:profesores')
+    return render(request, 'myapp/profesor_confirm_delete.html', {'profesor': profesor})
 
 def detalle_profesor(request, pk):
     profesor = get_object_or_404(Profesor, pk=pk)
